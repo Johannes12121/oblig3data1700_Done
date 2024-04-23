adf1690c069488db82aa0ef3c8ad44f560e2776c
Johannes12121 <josae4857@oslomet.no>
2024-04-23 14:46:41 +0200

    yyfh

---
+++ src/main/java/oslomettest1/oblig3done1/Oblig3Done1Application.java
@@ -0,0 +1,13 @@
+package oslomettest1.oblig3done1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3Done1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3Done1Application.class, args);
+    }
+
+}
---
+++ src/test/java/oslomettest1/oblig3done1/Oblig3Done1ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomettest1.oblig3done1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Done1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
